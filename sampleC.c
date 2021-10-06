0ca1bf889a0d97ff3912625887c2ad538483fbb5
127shubham <patilsshubham08@gmail.com>
2021-10-06 14:58:35 +0530

    first copy

--- sampleC.c
+++ sampleC.c
@@ -5,8 +5,9 @@ void addTwo()
 	int n1,n2;
 	printf("Enter Two integers");
 	scanf("%d,%d",&n1,&n2);
-	printf("sum of two numbers is %d",n1+n2);
+	printf("sum of %d and %d is %d",n1,n2,n1+n2);
 }
 void main()
 {
-	
+
+}
